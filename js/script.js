c3106ad1d90b85be446a789053ed522f09936838
Devgen <dev@generationyct.nl>
2020-02-21 13:31:00 +0100

    Added function to insert countries into DOM

--- js/script.js
+++ js/script.js
@@ -34,9 +34,51 @@ const countryFlags = [
 
 const listItems = []
 
+// Creating variable to store index for drag and drap API
+
+let dragStartIndex;
+
 // Creating variable to store countryFlags adding a index
 
 const countryByFlag = countryFlags.map((element) => element)
 
+// Invoke function createList to start first inserting into the DOM
 
+createList()
+
+// Inserting list items into the DOM
+
+function createList() {
+  // Make a shallow copy of countries array
+  [...countries]
+  // Changing methods on array copy
+    .map(a => ({ value: a, sort: Math.random() }))
+    .sort((a, b) => a.sort - b.sort)
+    .map(a => a.value)
+    .forEach((country, index) => {
+      const listItem = document.createElement('li')
+
+      listItem.setAttribute('data-index', index)
+
+      listItem.innerHTML = `
+
+        <span class="flag"><img src="${countryByFlag[index]}" width="150"></span>
+        <span class="number">${index + 1}</span>
+        <div class="draggable" draggable="true">
+        <p class="country-name">${country}</p>
+        <i><img src="./img/icondraganddrop.png" ></i>
+        </div>
+      `;
+
+      listItems.push(listItem)
+
+      draggable_list.appendChild(listItem)
+    })
+
+    addEventListeners()
+}
+
+function dragStart() {
+
+}
 
