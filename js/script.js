0a0df4fcd1214c53e3b8a6da90dc56bc22b93914
Devgen <dev@generationyct.nl>
2020-02-21 15:03:17 +0100

    Added extra styling

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 // Getting the DOM elements
 
 const draggable_list = document.getElementById('draggable-list')
+const check = document.getElementById('check');
 
 // Creating two arrays for use in project
 
@@ -62,12 +63,12 @@ function createList() {
 
       listItem.innerHTML = `
 
-        <span class="flag"><img src="${countryByFlag[index]}" width="150"></span>
-        <span class="number">${index + 1}</span>
+        <span class="flag"><img src="${countryByFlag[index]}"></span>
         <div class="draggable" draggable="true">
         <p class="country-name">${country}</p>
         <i><img src="./img/icondraganddrop.png" ></i>
         </div>
+        
       `;
 
       listItems.push(listItem)
@@ -79,6 +80,70 @@ function createList() {
 }
 
 function dragStart() {
-
+  // console.log('Event: ', 'dragstart');
+  dragStartIndex = +this.closest('li').getAttribute('data-index');
 }
 
+function dragEnter() {
+  // console.log('Event: ', 'dragenter');
+  this.classList.add('over');
+}
+
+function dragLeave() {
+  // console.log('Event: ', 'dragleave');
+  this.classList.remove('over');
+}
+
+function dragOver(e) {
+  // console.log('Event: ', 'dragover');
+  e.preventDefault();
+}
+
+function dragDrop() {
+  // console.log('Event: ', 'drop');
+  const dragEndIndex = +this.getAttribute('data-index');
+  swapItems(dragStartIndex, dragEndIndex);
+
+  this.classList.remove('over');
+}
+
+// Swap list items that are drag and drop
+function swapItems(fromIndex, toIndex) {
+  const itemOne = listItems[fromIndex].querySelector('.draggable');
+  const itemTwo = listItems[toIndex].querySelector('.draggable');
+
+  listItems[fromIndex].appendChild(itemTwo);
+  listItems[toIndex].appendChild(itemOne);
+}
+
+// Check the order of list items
+function checkOrder() {
+  listItems.forEach((listItem, index) => {
+    const countryName = listItem.querySelector('.draggable').innerText.trim();
+
+    if (countryName !== countries[index]) {
+      listItem.classList.add('wrong');
+    } else {
+      listItem.classList.remove('wrong');
+      listItem.classList.add('right');
+    }
+  });
+}
+
+function addEventListeners() {
+  const draggables = document.querySelectorAll('.draggable');
+  const dragListItems = document.querySelectorAll('.draggable-list li');
+
+  draggables.forEach(draggable => {
+    draggable.addEventListener('dragstart', dragStart);
+  });
+
+  dragListItems.forEach(item => {
+    item.addEventListener('dragover', dragOver);
+    item.addEventListener('drop', dragDrop);
+    item.addEventListener('dragenter', dragEnter);
+    item.addEventListener('dragleave', dragLeave);
+  });
+}
+
+check.addEventListener('click', checkOrder);
